c9ee152f9516c7c9e120bcbfe0d74cf8a690fd74
xerexodia <ibrahimfarhat2018@gmail.com>
2023-06-02 13:46:58 +0200

    applying user api's

--- src/pages/AdminDash/Users.js
+++ src/pages/AdminDash/Users.js
@@ -3,6 +3,7 @@ import 'styles/headerDash.scss';
 import { useNavigate } from 'react-router-dom';
 import Modal from 'components/Modal';
 import { Formik } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
 import {
     Button,
     Checkbox,
@@ -19,24 +20,45 @@ import {
     Typography
 } from '@mui/material';
 import AnimateButton from 'components/@extended/AnimateButton';
-
+import axios from 'axios';
 // assets
 import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
+import { url } from 'constants/urls';
+import { toInteger } from 'lodash';
+import { integer, trans } from 'utils/helpers';
+import Spinner from 'components/Spinner';
 
 // =========
 const Users = () => {
     const navigate = useNavigate();
     const [show, setShow] = useState(false);
     const [showPassword, setShowPassword] = React.useState(false);
-    const [checked, setChecked] = React.useState(false);
-
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
     const handleClickShowPassword = () => {
         setShowPassword(!showPassword);
     };
-
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
+    // ================ || api requests || =======================
+
+    // get all users
+    const getUsers = async () => {
+        setLoading(true);
+        return await axios.get(`${url}Profile/AllUsers`);
+    };
+    useEffect(() => {
+        getUsers()
+            .then((res) => {
+                setUsers(res.data), setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+            });
+        return () => {};
+    }, []);
     return (
         <div>
             <div className="dash-header">
@@ -53,22 +75,22 @@ const Users = () => {
                     <span>contracted</span>
                 </div>
                 <div className="users-grid-body">
-                    <div onClick={() => navigate('/dashboard/admin/users/qsdsdqsdsd')}>
-                        <span>#21qsd654321dq</span>
-                        <span>jihed</span>
-                        <span>jihed@gmail.com</span>
-                        <span>2236501523</span>
-                        <span>true</span>
-                        <span>true</span>
-                    </div>
-                    <div>
-                        <span>#21qsd654321dq</span>
-                        <span>jihed</span>
-                        <span>jihed@gmail.com</span>
-                        <span>2236501523</span>
-                        <span>true</span>
-                        <span>true</span>
-                    </div>
+                    {loading ? (
+                        <Spinner />
+                    ) : (
+                        <>
+                            {users?.map((user, idx) => (
+                                <div key={idx} onClick={() => navigate(`/dashboard/admin/users/${user.id}`)}>
+                                    <span>{user.id}</span>
+                                    <span>{user.userName}</span>
+                                    <span>{user.emailAddress}</span>
+                                    <span>{user.phoneNumber}</span>
+                                    <span>{trans(user.isAdmin)}</span>
+                                    <span>{trans(user.isContracted)}</span>
+                                </div>
+                            ))}
+                        </>
+                    )}
                 </div>
             </div>
             <Modal title="ajouter utilisateur" onClose={() => setShow(false)} show={show}>
@@ -76,10 +98,37 @@ const Users = () => {
                     initialValues={{
                         userName: '',
                         password: '',
-                        email: '',
-                        phone: '',
-                        admin: '',
-                        contracted: ''
+                        emailAddress: '',
+                        phoneNumber: '',
+                        isAdmin: false,
+                        isContracted: false
+                    }}
+                    onSubmit={async (values) => {
+                        try {
+                            setLoading(true);
+                            const data = await axios.post(`${url}Profile/AddUser`, {
+                                userName: values.userName,
+                                emailAddress: values.emailAddress,
+                                password: values.password,
+                                isAdmin: integer(values.isAdmin),
+                                isContracted: integer(values.isContracted),
+                                phoneNumber: values.phoneNumber
+                            });
+                            console.log(data);
+                            if (data.status == 200) {
+                                setUsers((prev) => [...prev, data.data]);
+                                setLoading(false);
+                                setShow(false);
+                                toast.success('user created successfully', {
+                                    autoClose: 2000
+                                });
+                            }
+                        } catch (error) {
+                            console.log(error);
+                            setLoading(false);
+                        } finally {
+                            setLoading(false);
+                        }
                     }}
                 >
                     {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
@@ -91,17 +140,17 @@ const Users = () => {
                                         <OutlinedInput
                                             id="email-login"
                                             type="email"
-                                            value={values.email}
-                                            name="email"
+                                            value={values.emailAddress}
+                                            name="emailAddress"
                                             onBlur={handleBlur}
                                             onChange={handleChange}
                                             placeholder="Enter email address"
                                             fullWidth
-                                            error={Boolean(touched.email && errors.email)}
+                                            error={Boolean(touched.emailAddress && errors.emailAddress)}
                                         />
-                                        {touched.email && errors.email && (
-                                            <FormHelperText error id="standard-weight-helper-text-email-login">
-                                                {errors.email}
+                                        {touched.emailAddress && errors.emailAddress && (
+                                            <FormHelperText error id="standard-weight-helper-text-emailAddress-login">
+                                                {errors.emailAddress}
                                             </FormHelperText>
                                         )}
                                     </Stack>
@@ -118,11 +167,11 @@ const Users = () => {
                                             onChange={handleChange}
                                             placeholder="Enter user name"
                                             fullWidth
-                                            error={Boolean(touched.email && errors.email)}
+                                            error={Boolean(touched.userName && errors.userName)}
                                         />
-                                        {touched.email && errors.email && (
-                                            <FormHelperText error id="standard-weight-helper-text-email-login">
-                                                {errors.email}
+                                        {touched.userName && errors.userName && (
+                                            <FormHelperText error id="standard-weight-helper-text-userName-login">
+                                                {errors.userName}
                                             </FormHelperText>
                                         )}
                                     </Stack>
@@ -133,17 +182,17 @@ const Users = () => {
                                         <OutlinedInput
                                             id="email-login"
                                             type="text"
-                                            value={values.phone}
-                                            name="phone"
+                                            value={values.phoneNumber}
+                                            name="phoneNumber"
                                             onBlur={handleBlur}
                                             onChange={handleChange}
-                                            placeholder="Enter email address"
+                                            placeholder="Enter num tel"
                                             fullWidth
-                                            error={Boolean(touched.email && errors.email)}
+                                            error={Boolean(touched.phoneNumber && errors.phoneNumber)}
                                         />
-                                        {touched.email && errors.email && (
-                                            <FormHelperText error id="standard-weight-helper-text-email-login">
-                                                {errors.email}
+                                        {touched.phoneNumber && errors.phoneNumber && (
+                                            <FormHelperText error id="standard-weight-helper-text-phoneNumber-login">
+                                                {errors.phoneNumber}
                                             </FormHelperText>
                                         )}
                                     </Stack>
@@ -189,12 +238,12 @@ const Users = () => {
                                                 <Checkbox
                                                     // checked={checked}
                                                     onChange={handleChange}
-                                                    name="admin"
+                                                    name="isAdmin"
                                                     color="primary"
                                                     size="small"
                                                 />
                                             }
-                                            label={<Typography variant="h6">Admin</Typography>}
+                                            label={<Typography variant="h6">admin</Typography>}
                                         />
                                     </Stack>
                                 </Grid>
@@ -205,12 +254,12 @@ const Users = () => {
                                                 <Checkbox
                                                     // checked={checked}
                                                     onChange={handleChange}
-                                                    name="contracted"
+                                                    name="isContracted"
                                                     color="primary"
                                                     size="small"
                                                 />
                                             }
-                                            label={<Typography variant="h6">contracted</Typography>}
+                                            label={<Typography variant="h6">isContracted</Typography>}
                                         />
                                     </Stack>
                                 </Grid>
@@ -240,6 +289,7 @@ const Users = () => {
                     )}
                 </Formik>
             </Modal>
+            <ToastContainer />
         </div>
     );
 };
